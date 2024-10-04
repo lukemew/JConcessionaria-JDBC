fc67d463e05cce8a01ad8c16df809bf6a0518f75
lukemew <lucasmelor65@gmail.com>
2024-10-04 10:34:06 -0300

    Corrigindo DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,16 +1,17 @@
 package model.dao;
 
 import db.DB;
-import model.dao.impl.;
-import model.dao.impl.ProdutoDaoJDBC;
+import model.dao.impl.ClienteDaoJDBC;
 import model.dao.impl.VendedorDaoJDBC;
 
 public class DaoFactory {
 
     public static VendedorDao createVendedorDao() {
-        return new ProdutoDaoJDBC(DB.getConnection());
-        return new ClienteDaoJDBC(DB.getConnection());
-        return new VendedorDaoJDBC(DB.getConnection()); {
-        }
+        return new VendedorDaoJDBC(DB.getConnection());
     }
+
+    public static ClienteDao createClienteDao() {
+        return new ClienteDaoJDBC(DB.getConnection());
+    }
+
 }
